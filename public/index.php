004d7eb0f154e3d922f6bdb39410b9eff561673e
disastersystem <robin.vigdal.bekkevold@gmail.com>
2017-07-26 01:02:37 +0200

    Test.

--- public/index.php
+++ public/index.php
@@ -6,19 +6,17 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+		<link href='https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700|Material+Icons' rel="stylesheet" type="text/css">
 		<link rel="stylesheet" type="text/css" href="site/fabulous.css">
 	</head>
 	<body>
 		<div class="container">
-			<div class="top">
-				<h2 class="words">why though</h2>
-			</div>
-			
-			<div class="bottom">
-				<img src="site/img/why.png" class="image">
-				<img src="site/img/oh.png" class="image">
-			</div>
+			<a href="https://github.com/disastersystem">
+				github
+				<i class="material-icons">
+					open_in_new
+				</i>
+			</a>
 		</div>
 	</body>
 </html>
